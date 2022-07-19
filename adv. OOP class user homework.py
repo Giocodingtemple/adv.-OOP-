7f5684df44c7cede9de437f4bd611c6092883366
Giocodingtemple <gmarchiori5149@gmail.com>
2022-07-18 21:16:10 -0500

    first commit

--- adv. OOP class user homework.py
+++ adv. OOP class user homework.py
@@ -44,9 +44,7 @@ class User():
     def last_name_change(self):
         self.last_name.append()
         return input(f"new last name")        
-            
-
-
+          
 class Employee(User):
     def __init__(self, first_name, last_name, email, home_address, security_level, department, id):
         super().__init__(self, first_name, last_name, email, id)
@@ -55,7 +53,6 @@ class Employee(User):
         self.department = department
         self.created_on = dt.now().strftime("%c")
 
-
     @property
     def id(self):
         return self.first_name + '.' + self.last_name + '.' + self.department
@@ -63,8 +60,6 @@ class Employee(User):
     def department_change(self):
         self.department.append()
         return input(f"new department section")
-    
-
     
 class Customer(User):
     def __init__(self, first_name, last_name, email, shipping_address, billing_address, purchase_history, id):
@@ -85,8 +80,6 @@ class Customer(User):
     def shipping_address_change(self):
         self.shipping_address.append()
         return input(f"new shipping address")
-
-
 
 andrea = Employee("andrea", "Harrison", "aharrison@walkingdead.com", "220 Victoria Trace, Senoia, GA 30276",  3, "guns",  "harrisonandrea,guns") 
 beth = Employee("beth", "greene", "bgreene@walkingdead.com", "220 Victoria Trace, Senoia, GA 30276",  5, "Morgue technician", "greenebeth, morguetechician") 
